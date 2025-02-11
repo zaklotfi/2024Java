94862cbabe6425eb169b07b1fd0ccf5ace159136
Zakaria <zikolotfi1924@gmail.com>
2025-02-11 15:46:34 +0100

    depot

--- MAIN.java
+++ MAIN.java
@@ -1,12 +1,28 @@
-package exo1;
-
 public class Main {
     public static void main(String[] args) {
-        Voiture voiture = new Voiture();
-        voiture.marque = "Toyota";
-        voiture.modele = "Corolla";
-        voiture.couleur = "Rouge";
+        // Demande des infos à l'utilisateur
+        Scanner scanner = new Scanner(System.in); // Création de l'objet Scanner
+        System.out.print("Entrez la couleur : ");
+        String couleur = scanner.nextLine();
+        scanner.close(); // Fermeture du scanner
 
-        voiture.demarrer();
+        boolean found = false;
+        for(String color : Voiture.colorAutorized) {
+            if (color.equals(couleur)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            // Création de la voiture
+            Voiture voiture = new Voiture("Toyota", "Corolla", "Rouge");
+            
+            // Affichage des infos de la voiture
+            System.out.println(voiture.getMarque() + " " + voiture.getModele() + " " + voiture.getCouleur());
+        } else {
+            System.out.println("Erreur dans le choix de la couleur !");
+            System.exit(1);
+        }
     }
 }
